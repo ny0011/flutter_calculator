e31d959cf14c17a6166e20c29967b4fb96b0acf8
ny0011 <ny0011@github.com>
2019-04-16 00:37:20 +0900

    add example code
    Changing widgets in response to input
    -> 이벤트가 발생했을 때 상태를 바꾸는 방법

--- lib/main.dart
+++ lib/main.dart
@@ -111,6 +111,73 @@ void main() {
   ));
 }
 
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  // This class is the configuration for the state. It holds the
+  // values (in this case nothing) provided by the parent and used by the build
+  // method of the State. Fields in a Widget subclass are always marked "final".
+
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If we changed _counter without calling
+      // setState(), then the build method would not be called again,
+      // and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance
+    // as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
+        ),
+        Text('Count: $_counter'),
+      ],
+    );
+  }
+}
+
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -120,26 +187,35 @@ class TutorialHome extends StatelessWidget {
         leading: IconButton(
           icon: Icon(Icons.menu),
           tooltip: 'Navigation menu',
-          onPressed: null,
+          onPressed: ()
+          {
+            print('nav menu');
+          },
         ),
         title: Text('Example title'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
             tooltip: 'Search',
-            onPressed: null,
+            onPressed: ()
+            {
+              print('search button');
+            },
           ),
         ],
       ),
       // body is the majority of the screen.
-      body: Center(
-        child: Text('Hello, world!'),
-      ),
+      body:  Counter(),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
         child: Icon(Icons.add),
-        onPressed: null,
+        onPressed: ()
+        {
+          print('add button');
+        },
       ),
+      bottomNavigationBar:
+        MyButton(),
     );
   }
 }
